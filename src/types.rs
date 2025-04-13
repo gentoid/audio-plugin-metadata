2a9af8fdcaa8ed7e0648f3ed98dd269a48d535e4
Viktor Lazarev <taurus101v@gmail.com>
2025-04-13 18:58:41 +0200

    Improve the logic getting plugin info

--- src/types.rs
+++ src/types.rs
@@ -1,29 +1,21 @@
+#[derive(Debug)]
 pub enum PluginFormat {
     Vst2,
 }
 
+#[derive(Debug)]
 pub struct PluginInfo {
     pub path: String,
-    pub name: String,
+    pub name: Option<String>,
     pub vendor: Option<String>,
     pub version: Option<String>,
-    pub product: Option<String>,
-    pub unique_id: Option<u32>,
-    pub inputs: Option<u32>,
-    pub outputs: Option<u32>,
-    pub parameters: Option<u32>,
-    pub presets: Option<u32>,
-    pub is_synth: Option<bool>,
-    pub has_editor: Option<bool>,
+    pub unique_id: u32,
+    pub inputs: u32,
+    pub outputs: u32,
+    pub parameters: u32,
+    pub presets: u32,
+    pub is_synth: bool,
+    pub has_editor: bool,
     pub category: Option<String>,
     pub format: PluginFormat,
-}
-
-#[repr(i32)]
-pub enum VstDispatcherOpcode {
-    EffGetEffectName = 32,
-    EffGetVendorString = 33,
-    EffGetProductString = 34,
-    EffGetVendorVersion = 35,
-    EffGetPlugCategory = 36,
 }
--- src/vst2.rs
+++ src/vst2.rs
@@ -1,24 +1,43 @@
 use std::{ffi::CStr, os::raw::c_void, path::Path, ptr::NonNull};
 
 use libloading::{Library, Symbol};
-use vst2_sys::AEffect;
+use vst2_sys::{AEffect, HostCallbackProc, effect_opcodes as opcode};
 
-use crate::types::{PluginFormat, PluginInfo, VstDispatcherOpcode as opcode};
+use crate::types::{PluginFormat, PluginInfo};
 
 type VstIntPtr = isize;
-type HostCallback =
-    unsafe extern "C" fn(*mut AEffect, i32, i32, VstIntPtr, *mut c_void, f32) -> VstIntPtr;
-type VstMain = unsafe extern "C" fn(callback: HostCallback) -> *mut AEffect;
+type VstMain = unsafe extern "C" fn(callback: HostCallbackProc) -> *mut AEffect;
 
-unsafe extern "C" fn dummy_host_callback(
+extern "C" fn dummy_host_callback(
     _effect: *mut AEffect,
-    _opcode: i32,
+    opcode: i32,
     _index: i32,
     _value: VstIntPtr,
-    _ptr: *mut c_void,
+    ptr: *mut c_void,
     _opt: f32,
 ) -> VstIntPtr {
-    0
+    println!("Passed opcode: {opcode}");
+    match opcode {
+        1 => 2100, // audioMasterVersion
+        33 => {
+            // audioMasterGetVendorString
+            let s = b"MyHost\0";
+            unsafe {
+                std::ptr::copy_nonoverlapping(s.as_ptr(), ptr as *mut u8, s.len());
+            }
+            1
+        }
+        34 => {
+            // audioMasterGetProductString
+            let s = b"MyHostProduct\0";
+            unsafe {
+                std::ptr::copy_nonoverlapping(s.as_ptr(), ptr as *mut u8, s.len());
+            }
+            1
+        }
+        35 => 1000, // audioMasterGetVendorVersion
+        _ => 0,
+    }
 }
 
 pub fn scan_vst2(path: &Path) -> Result<PluginInfo, Box<dyn std::error::Error>> {
@@ -31,35 +50,43 @@ pub fn scan_vst2(path: &Path) -> Result<PluginInfo, Box<dyn std::error::Error>> 
     let effect = NonNull::new(effect).ok_or("effect is null")?;
     let eff = unsafe { effect.as_ref() };
 
-    let name =
-        get_string(eff, opcode::EffGetEffectName as i32).ok_or("Plugin doesn't have name")?;
-    let vendor = get_string(eff, opcode::EffGetVendorString as i32);
-    let product = get_string(eff, opcode::EffGetProductString as i32);
-    let version = get_string(eff, opcode::EffGetVendorVersion as i32);
-    let category = get_string(eff, opcode::EffGetPlugCategory as i32);
+    ((eff.dispatcher)(
+        eff as *const _ as *mut AEffect,
+        opcode::OPEN,
+        0,
+        0,
+        std::ptr::null_mut(),
+        0.0,
+    ));
+
+    let name = get_string(eff, opcode::GET_EFFECT_NAME)
+        .or_else(|| get_string(eff, opcode::GET_PRODUCT_STRING));
+    let vendor = get_string(eff, opcode::GET_VENDOR_STRING);
+    let version = get_string(eff, opcode::GET_VENDOR_VERSION);
+    let category = get_string(eff, opcode::GET_PLUG_CATEGORY);
 
     Ok(PluginInfo {
         path: path.display().to_string(),
         name,
         vendor,
         version,
-        product,
-        unique_id: Some(eff.unique_id as u32),
-        inputs: Some(eff.num_inputs as u32),
-        outputs: Some(eff.num_outputs as u32),
-        parameters: Some(eff.num_params as u32),
-        presets: Some(eff.num_programs as u32),
-        is_synth: Some(eff.flags & (1 << 8) != 0),
-        has_editor: Some(eff.flags & (1 << 0) != 0),
         category,
+        unique_id: eff.unique_id as u32,
+        inputs: eff.num_inputs as u32,
+        outputs: eff.num_outputs as u32,
+        parameters: eff.num_params as u32,
+        presets: eff.num_programs as u32,
+        is_synth: eff.flags & (1 << 8) != 0,
+        has_editor: eff.flags & (1 << 0) != 0,
         format: PluginFormat::Vst2,
     })
 }
+
 fn get_string(eff: &AEffect, opcode: i32) -> Option<String> {
-    let mut buffer = [0i8; 256];
+    let mut buffer = [0i8; 64];
     let dispatcher = eff.dispatcher;
 
-    let result = dispatcher(
+    let result: VstIntPtr = dispatcher(
         eff as *const _ as *mut AEffect,
         opcode,
         0,
@@ -72,9 +99,13 @@ fn get_string(eff: &AEffect, opcode: i32) -> Option<String> {
         return None;
     }
 
-    Some(
-        unsafe { CStr::from_ptr(buffer.as_ptr()) }
-            .to_string_lossy()
-            .into_owned(),
-    )
+    let string = unsafe { CStr::from_ptr(buffer.as_ptr()) }
+        .to_string_lossy()
+        .into_owned();
+
+    if string.is_empty() {
+        return None;
+    }
+
+    Some(string)
 }
